c7ab2cdca565dd3adc53533380d00ffb80f47b0d
ahguerraViridian <andres.heredia@viridian.cc>
2022-02-01 09:29:03 -0400

    added mian dev

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,19 @@
 import 'dart:io';
+
+import 'runAppMethod.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'appSpectorConfig.dart';
 import 'clientConfig.dart';
-import 'app.dart';
+import 'app.dart' as app;
 import 'themes/appThemes.dart';
 import 'utils/allUtils.dart';
 import 'config/config.dart';
 
 void main() async {
-  Config.appFlavor = Flavor.PROD;
+  Config.appFlavor = Flavor.DEV;
   Config.appClient = appClient;
   Config.iOSAppID = iOSAppID;
   AppThemes.setStatusBarColor();
@@ -20,5 +23,12 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SharedPreferences.getInstance();
   SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
-  runApp(new App());
+
+  /// END
+  runAppSpector(Config.appFlavor);
+  runAppMethod(() {
+    runApp(
+      app.App(),
+    );
+  });
 }
